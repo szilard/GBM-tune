814cc7aba79672d09cfdca788c9be44393e74eba
szilard <szilard@users.noreply.github.com>
2017-08-21 21:10:03 -0700

    more

--- tune.R
+++ tune.R
@@ -12,11 +12,13 @@ set.seed(123)
 yr <- 1991
 
 d0_train <- fread(paste0("/var/data/airline/",yr-1,".csv"))
+d0_train <- d0_train[!is.na(DepDelay)]
+
 d0_test <- fread(paste0("/var/data/airline/",yr,".csv"))
+d0_test <- d0_test[!is.na(DepDelay)]
+
 d0 <- rbind(d0_train, d0_test)
 
-
-d0 <- d0[!is.na(DepDelay)]
 
 for (k in c("Month","DayofMonth","DayOfWeek")) {
   d0[[k]] <- paste0("c-",as.character(d0[[k]]))
@@ -61,8 +63,14 @@ params_grid <- expand.grid(                      # default
 params_random <- params_grid[sample(1:nrow(params_grid),n_random),]
 
 
+
 ## TODO: resample 
 d <- d1_train[sample(1:nrow(d1_train), size),]
+
+
+
+## TODO: resample 
+d_test <- d1_test[sample(1:nrow(d1_test), 100e3),]
 
 
 
@@ -89,8 +97,9 @@ for (k in 1:n_resample) {
   
   
   for (krpm in 1:nrow(params_random)) {
-    cat("k:",kfold," krpm:",krpm,"\n")
+    cat("k:",k," krpm:",krpm,"\n")
     params <- as.list(params_random[krpm,])
+    
     runtm <- system.time({
       md <- lgb.train(data = dlgb_train, objective = "binary",
                       params = params,
@@ -98,23 +107,32 @@ for (k in 1:n_resample) {
                       categorical_feature = cols_cats, 
                       verbose = 0)
     })[[3]]
+    
     phat <- predict(md, data = d_modelselec[,1:p])
     rocr_pred <- prediction(phat, d_modelselec[,p+1])
-    auc <- performance(rocr_pred, "auc")@y.values[[1]]
-    d_res <- rbind(d_res, data.frame(krpm = krpm, k = k, ntrees = md$best_iter, runtm = runtm, auc = auc))
+    auc_rs <- performance(rocr_pred, "auc")@y.values[[1]]
+    
+    phat <- predict(md, data = d_test[,1:p])
+    rocr_pred <- prediction(phat, d_test[,p+1])
+    auc_test <- performance(rocr_pred, "auc")@y.values[[1]]
+    
+    d_res <- rbind(d_res, data.frame(krpm = krpm, k = k, ntrees = md$best_iter, runtm = runtm, 
+                                     auc_rs = auc_rs, auc_test = auc_test))
   }
   
 }
 
-d_res_avg <- d_res %>% group_by(krpm) %>% summarize(ntrees = mean(ntrees), runtm = mean(runtm), auc = mean(auc))
+d_res_avg <- d_res %>% group_by(krpm) %>% summarize(ntrees = mean(ntrees), runtm = mean(runtm), 
+                                                    auc_rs = mean(auc_rs), auc_test = mean(auc_test))
 d_pm_res <- cbind(params_random, d_res_avg)
 
 d_pm_res
 
 
 
-d_pm_res %>% arrange(desc(auc)) %>% head(10)
+d_pm_res %>% arrange(desc(auc_rs)) %>% head(10)
 
-d_pm_res %>% mutate(rank = dense_rank(desc(auc))) %>% ggplot() + geom_point(aes(x = rank, y = auc))
+d_pm_res %>% mutate(rank = dense_rank(desc(auc_rs))) %>% ggplot() + geom_point(aes(x = rank, y = auc_rs))
 
+d_pm_res %>% ggplot() + geom_point(aes(x = auc_rs, y = auc_test))
 
