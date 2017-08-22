1e62e91b75afcef717bdb24e112feaaf99953873
szilard <szilard@users.noreply.github.com>
2017-08-22 16:49:16 -0700

    res 40 resamples

--- run-tuning.R
+++ run-tuning.R
@@ -31,7 +31,7 @@ for (k in c("Month","DayofMonth","DayOfWeek")) {
 d0[["dep_delayed_15min"]] <- ifelse(d0[["DepDelay"]]>=15,1,0) 
 
 cols_keep <- c("Month", "DayofMonth", "DayOfWeek", "DepTime", "UniqueCarrier", 
-               "Origin", "Dest", "Distance", "dep_delayed_15min")
+          "Origin", "Dest", "Distance", "dep_delayed_15min")
 d0 <- d0[, cols_keep, with = FALSE]
 
 
@@ -57,14 +57,14 @@ size <- 100e3
 n_random <- 100      ## TODO: 1000?
 
 params_grid <- expand.grid(                      # default
-  num_leaves = c(30,100,200,500,1000,2000,5000), # 31 (was 127)
+  num_leaves = c(100,200,500,1000,2000,5000),    # 31 (was 127)
   learning_rate = c(0.01,0.03,0.1),              # 0.1
   min_data_in_leaf = c(5,10,20,50),              # 20 (was 100)
   feature_fraction = c(0.6,0.8,1),               # 1 
-  bagging_fraction = c(0.6,0.8,1),               # 1
+  bagging_fraction = c(0.4,0.6,0.8,1),           # 1
   lambda_l1 = c(0,0,0,0, 0.01, 0.1, 0.3),        # 0
   lambda_l2 = c(0,0,0,0, 0.01, 0.1, 0.3)         # 0
-  ## TODO:  
+## TODO:  
   ## min_sum_hessian_in_leaf
   ## min_gain_to_split
   ## max_bin
@@ -107,67 +107,67 @@ system.time({
 
 
 system.time({
-  
-  d_res <- data.frame()
-  for (krpm in 1:nrow(params_random)) {
-    params <- as.list(params_random[krpm,])
+
+d_res <- data.frame()
+for (krpm in 1:nrow(params_random)) {
+  params <- as.list(params_random[krpm,])
     
-    ## resample
-    n_resample <- 20     ## TODO: 10?
+  ## resample
+  n_resample <- 20     ## TODO: 10?
+
+  p_train <- 0.8       ## TODO: change? (80-10-10 split now)
+  p_earlystop <- 0.1   
+  p_modelselec <- 1 - p_train - p_earlystop
+
+  mds <- list()
+  d_res_rs <- data.frame()
+  for (k in 1:n_resample) {
+    cat(" krpm:",krpm,"k:",k,"\n")
     
-    p_train <- 0.8       ## TODO: change? (80-10-10 split now)
-    p_earlystop <- 0.1   
-    p_modelselec <- 1 - p_train - p_earlystop
+    idx_train      <- sample(1:size, size*p_train)
+    idx_earlystop  <- sample(setdiff(1:size,idx_train), size*p_earlystop)
+    idx_modelselec <- setdiff(setdiff(1:size,idx_train),idx_earlystop)
     
-    mds <- list()
-    d_res_rs <- data.frame()
-    for (k in 1:n_resample) {
-      cat(" krpm:",krpm,"k:",k,"\n")
-      
-      idx_train      <- sample(1:size, size*p_train)
-      idx_earlystop  <- sample(setdiff(1:size,idx_train), size*p_earlystop)
-      idx_modelselec <- setdiff(setdiff(1:size,idx_train),idx_earlystop)
-      
-      d_train <- d[idx_train,]
-      d_earlystop <- d[idx_earlystop,]
-      d_modelselec <- d[idx_modelselec,]
-      
-      dlgb_train     <- lgb.Dataset(data = d_train[,1:p],     label = d_train[,p+1])
-      dlgb_earlystop <- lgb.Dataset(data = d_earlystop[,1:p], label = d_earlystop[,p+1])
-      
-      
-      runtm <- system.time({
+    d_train <- d[idx_train,]
+    d_earlystop <- d[idx_earlystop,]
+    d_modelselec <- d[idx_modelselec,]
+    
+    dlgb_train     <- lgb.Dataset(data = d_train[,1:p],     label = d_train[,p+1])
+    dlgb_earlystop <- lgb.Dataset(data = d_earlystop[,1:p], label = d_earlystop[,p+1])
+
+    
+    runtm <- system.time({
         md <- lgb.train(data = dlgb_train, objective = "binary",
-                        num_threads = parallel::detectCores()/2,     # = number of "real" cores
-                        params = params,
-                        nrounds = 10000, early_stopping_rounds = 10, valid = list(valid = dlgb_earlystop), 
-                        categorical_feature = cols_cats, 
-                        verbose = 0)
-      })[[3]]
-      
-      phat <- predict(md, data = d_modelselec[,1:p])
-      rocr_pred <- prediction(phat, d_modelselec[,p+1])
-      auc_rs <- performance(rocr_pred, "auc")@y.values[[1]]
-      
-      d_res_rs <- rbind(d_res_rs, data.frame(ntrees = md$best_iter, runtm = runtm, auc_rs = auc_rs))
-      mds[[k]] <- md
-    }  
-    d_res_rs_avg <- d_res_rs %>% summarize(ntrees = mean(ntrees), runtm = mean(runtm), auc_rs_avg = mean(auc_rs),
-                                           auc_rs_std = sd(auc_rs)/sqrt(n_resample))   # std of the mean!
+              num_threads = parallel::detectCores()/2,     # = number of "real" cores
+              params = params,
+              nrounds = 10000, early_stopping_rounds = 10, valid = list(valid = dlgb_earlystop), 
+              categorical_feature = cols_cats, 
+              verbose = 0)
+    })[[3]]
     
-    # consider the model as the average of the models from resamples 
-    # TODO?: alternatively could retrain the "final" model on all of data (early stoping or avg number of trees?)
-    phat <- matrix(0, nrow = n_resample, ncol = nrow(d_test))
-    for (k in 1:n_resample) {
-      phat[k,] <- predict(mds[[k]], data = d_test[,1:p])
-    }
-    phat_avg <- apply(phat, 2, mean)   
-    rocr_pred <- prediction(phat_avg, d_test[,p+1])
-    auc_test <- performance(rocr_pred, "auc")@y.values[[1]]
+    phat <- predict(md, data = d_modelselec[,1:p])
+    rocr_pred <- prediction(phat, d_modelselec[,p+1])
+    auc_rs <- performance(rocr_pred, "auc")@y.values[[1]]
     
-    d_res <- rbind(d_res, cbind(krpm, d_res_rs_avg, auc_test))
-  }
+    d_res_rs <- rbind(d_res_rs, data.frame(ntrees = md$best_iter, runtm = runtm, auc_rs = auc_rs))
+    mds[[k]] <- md
+  }  
+  d_res_rs_avg <- d_res_rs %>% summarize(ntrees = mean(ntrees), runtm = mean(runtm), auc_rs_avg = mean(auc_rs),
+                                         auc_rs_std = sd(auc_rs)/sqrt(n_resample))   # std of the mean!
   
+  # consider the model as the average of the models from resamples 
+  # TODO?: alternatively could retrain the "final" model on all of data (early stoping or avg number of trees?)
+  phat <- matrix(0, nrow = n_resample, ncol = nrow(d_test))
+  for (k in 1:n_resample) {
+    phat[k,] <- predict(mds[[k]], data = d_test[,1:p])
+  }
+  phat_avg <- apply(phat, 2, mean)   
+  rocr_pred <- prediction(phat_avg, d_test[,p+1])
+  auc_test <- performance(rocr_pred, "auc")@y.values[[1]]
+  
+  d_res <- rbind(d_res, cbind(krpm, d_res_rs_avg, auc_test))
+}
+
 })
 
 d_pm_res <- cbind(params_random, d_res)
