e4d0e6b2f91a8f97878f85a77456b10d63f11374
szilard <szilard@users.noreply.github.com>
2017-08-21 21:36:29 -0700

    more

--- tune.R
+++ tune.R
@@ -49,7 +49,7 @@ d1_test  <- as.matrix(d0[(n1+1):(n1+n2),])
 size <- 100e3
 
 
-n_random <- 10
+n_random <- 100
 
 params_grid <- expand.grid(                      # default
   num_leaves = c(10,20,30,50,100,200,500,1000),  # 31 (was 127)
@@ -74,53 +74,57 @@ d_test <- d1_test[sample(1:nrow(d1_test), 100e3),]
 
 
 
-## resample
-n_resample <- 5
-
-p_train <- 0.6
-p_earlystop <- 0.2
-p_modelselec <- 1 - p_train - p_earlystop
-
-d_res <- data.frame()
-for (k in 1:n_resample) {
+system.time({
   
-  idx_train      <- sample(1:size, size*p_train)
-  idx_earlystop  <- sample(setdiff(1:size,idx_train), size*p_earlystop)
-  idx_modelselec <- setdiff(setdiff(1:size,idx_train),idx_earlystop)
+  ## resample
+  n_resample <- 20
   
-  d_train <- d[idx_train,]
-  d_earlystop <- d[idx_earlystop,]
-  d_modelselec <- d[idx_modelselec,]
+  p_train <- 0.6
+  p_earlystop <- 0.2
+  p_modelselec <- 1 - p_train - p_earlystop
   
-  dlgb_train     <- lgb.Dataset(data = d_train[,1:p],     label = d_train[,p+1])
-  dlgb_earlystop <- lgb.Dataset(data = d_earlystop[,1:p], label = d_earlystop[,p+1])
-  
-  
-  for (krpm in 1:nrow(params_random)) {
-    cat("k:",k," krpm:",krpm,"\n")
-    params <- as.list(params_random[krpm,])
+  d_res <- data.frame()
+  for (k in 1:n_resample) {
     
-    runtm <- system.time({
-      md <- lgb.train(data = dlgb_train, objective = "binary",
-                      params = params,
-                      nrounds = 10000, early_stopping_rounds = 10, valid = list(valid = dlgb_earlystop), 
-                      categorical_feature = cols_cats, 
-                      verbose = 0)
-    })[[3]]
+    idx_train      <- sample(1:size, size*p_train)
+    idx_earlystop  <- sample(setdiff(1:size,idx_train), size*p_earlystop)
+    idx_modelselec <- setdiff(setdiff(1:size,idx_train),idx_earlystop)
     
-    phat <- predict(md, data = d_modelselec[,1:p])
-    rocr_pred <- prediction(phat, d_modelselec[,p+1])
-    auc_rs <- performance(rocr_pred, "auc")@y.values[[1]]
+    d_train <- d[idx_train,]
+    d_earlystop <- d[idx_earlystop,]
+    d_modelselec <- d[idx_modelselec,]
     
-    phat <- predict(md, data = d_test[,1:p])
-    rocr_pred <- prediction(phat, d_test[,p+1])
-    auc_test <- performance(rocr_pred, "auc")@y.values[[1]]
+    dlgb_train     <- lgb.Dataset(data = d_train[,1:p],     label = d_train[,p+1])
+    dlgb_earlystop <- lgb.Dataset(data = d_earlystop[,1:p], label = d_earlystop[,p+1])
     
-    d_res <- rbind(d_res, data.frame(krpm = krpm, k = k, ntrees = md$best_iter, runtm = runtm, 
-                                     auc_rs = auc_rs, auc_test = auc_test))
+    
+    for (krpm in 1:nrow(params_random)) {
+      cat("k:",k," krpm:",krpm,"\n")
+      params <- as.list(params_random[krpm,])
+      
+      runtm <- system.time({
+        md <- lgb.train(data = dlgb_train, objective = "binary",
+                        params = params,
+                        nrounds = 10000, early_stopping_rounds = 10, valid = list(valid = dlgb_earlystop), 
+                        categorical_feature = cols_cats, 
+                        verbose = 0)
+      })[[3]]
+      
+      phat <- predict(md, data = d_modelselec[,1:p])
+      rocr_pred <- prediction(phat, d_modelselec[,p+1])
+      auc_rs <- performance(rocr_pred, "auc")@y.values[[1]]
+      
+      phat <- predict(md, data = d_test[,1:p])
+      rocr_pred <- prediction(phat, d_test[,p+1])
+      auc_test <- performance(rocr_pred, "auc")@y.values[[1]]
+      
+      d_res <- rbind(d_res, data.frame(krpm = krpm, k = k, ntrees = md$best_iter, runtm = runtm, 
+                                       auc_rs = auc_rs, auc_test = auc_test))
+    }
+    
   }
   
-}
+})
 
 d_res_avg <- d_res %>% group_by(krpm) %>% summarize(ntrees = mean(ntrees), runtm = mean(runtm), 
                                                     auc_rs = mean(auc_rs), auc_test = mean(auc_test))
